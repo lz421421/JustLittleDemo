588f791d901dcf5ca4ef1bbe3d8b1d76b7707184
李志 <li.zhi6@jyall>
2016-11-22 15:12:04 +0800

    RecycleView上下啦刷新

---
+++ app/src/main/java/com/lizhi/demo/recyleView/OnFlashLoadMoreListener.java
@@ -0,0 +1,10 @@
+package com.lizhi.demo.recyleView;
+
+/**
+ * Created by 39157 on 2016/11/21.
+ */
+
+public interface OnFlashLoadMoreListener {
+    public void onFlash();
+    public void onLoadMore();
+}
